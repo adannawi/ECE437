8ec35c98092d86e5e4f6aa3bafb9389a9c42714d
Noah Petersen <peter248@purdue.edu>
2017-03-06 17:51:46 -0500

    Minor fixes

--- pipeline/source/dcache.sv
+++ pipeline/source/dcache.sv
@@ -1,9 +1,16 @@
 
 `include "cpu_types_pkg.vh"
-`include "cache_control_if.vh"
+`include "caches_if.vh"
 
-module icache (
+module dcache (
 	caches_if.dcache icif
 	);
+
+  // dcache ports
+  //modport dcache (
+  //  input   halt, dmemREN, dmemWEN,
+  //          datomic, dmemstore, dmemaddr,
+  //  output  dhit, dmemload, flushed
+  //);
 
 endmodule
--- pipeline/source/icache.sv
+++ pipeline/source/icache.sv
@@ -1,8 +1,14 @@
-`include "cache_control_if.vh"
+`include "caches_if.vh"
 `include "cpu_types_pkg.vh"
 
 module icache (
 	caches_if.icache icif
 	);
 
+  // icache ports
+  //dport icache (
+  //  input   imemREN, imemaddr,
+  //  output  ihit, imemload
+  //);
+  
 endmodule
