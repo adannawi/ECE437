a0c47beb88a82f4acb5a1e4f45b2b8a0b3c4b03d
Noah Petersen <peter248@purdue.edu>
2017-03-06 17:21:24 -0500

    Actually put stuff in there now

---
+++ pipeline/source/dcache.sv
@@ -0,0 +1,9 @@
+
+`include "cpu_types_pkg.vh"
+`include "cache_control_if.vh"
+
+module icache (
+	caches_if.dcache icif
+	);
+
+endmodule
---
+++ pipeline/source/icache.sv
@@ -0,0 +1,8 @@
+`include "cache_control_if.vh"
+`include "cpu_types_pkg.vh"
+
+module icache (
+	caches_if.icache icif
+	);
+
+endmodule
